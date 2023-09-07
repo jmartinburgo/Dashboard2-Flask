59be57edcb20b82589fc9a20b5430e9de64d8768
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2023-09-07 18:51:31 -0300

    Update

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///expensesDB.db'
+app.config['SECRET_KEY']='joasjdoaiooajdoaisjdoaiendjasoidjasoidjkjoasijdoaisdjoasijdaosidjaoisd'
+
+
+db=SQLAlchemy(app)
 
 
 from application import routes
