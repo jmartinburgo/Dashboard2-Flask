5403dcf6793c8cca792904f8573bf71f6fdc0840
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2023-09-08 18:12:59 -0300

    update

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///expensesDB.db'
 app.config['SECRET_KEY']='joasjdoaiooajdoaisjdoaiendjasoidjasoidjkjoasijdoaisdjoasijdaosidjaoisd'
-
+ 
 
 db=SQLAlchemy(app)
 
--- application/models.py
+++ application/models.py
@@ -10,3 +10,5 @@ class IncomeExpenses(db.Model):
 
     def __str__(self):
         return self.id
+    
+    def 
